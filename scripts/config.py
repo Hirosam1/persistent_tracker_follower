2a9d8158f99a87c7314f69650cc0f7a28fa397da
Samuel Pizzatto <samuel.pizzatto@gmail.com>
2026-07-22 09:56:13 -0300

    rename model folder

--- scripts/config.py
+++ scripts/config.py
@@ -8,7 +8,7 @@ except Exception:
     _pkg_dir = Path(__file__).resolve().parent.parent
 
 #MODEL_PATH = os.path.join(_pkg_dir, 'models', 'yolo11n.pt')
-MODEL_PATH = os.path.join(_pkg_dir, 'models', 'yolo_openvino')
+MODEL_PATH = os.path.join(_pkg_dir, 'models', 'yolo11n_openvino_model')
 
 PATH_TO_OSNET_MODEL = os.path.join(_pkg_dir, 'models', 'osnet_x0_25_msmt17.pth')
 
